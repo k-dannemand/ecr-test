3610b2846f00556c5aa7689ed45b4e86b67d464a
k-dannemand <kd@kdnm.dk>
2021-06-11 10:47:23 +0200

    add test repo

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 module "pastie" {
   source  = "./module/"
   name    = "pastie"
+}
+
+module "test-repo" {
+  source  = "./module/"
+  name    = "test-repo"
   iam_arn = "arn:aws:iam::986468747188:user/kd"
 }
